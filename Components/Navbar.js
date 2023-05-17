f49383bcafc735efe90e14886317ecb9be641b16
Harsh517-tech415 <83271527+Harsh517-tech415@users.noreply.github.com>
2023-05-17 17:59:12 +0530

    Navbar-1

---
+++ Components/Navbar.js
@@ -0,0 +1,16 @@
+import { AppBar, Box, CardContent, Container, Stack, Toolbar, Typography } from '@mui/material'
+import Link from 'next/link'
+import React from 'react'
+const Navbar = () => {
+  return (
+    <AppBar>
+        <Container maxWidth="xl" >
+            <Toolbar disableGutters>    
+            
+            </Toolbar>
+        </Container>
+    </AppBar>
+  )
+}
+
+export default Navbar
